e8614fa2f79f279365e1fb45c50bdaf85e589ffd
Wallace <wallacesb@outlook.com.br>
2023-06-23 22:39:21 -0300

    Finalizando app Angular

--- lista11-introducaoAngular/ex01/src/app/app.component.ts
+++ lista11-introducaoAngular/ex01/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Produto from './models/produto';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ex01';
+  public produtos:  Produto[] = new Array<Produto>();
+
+  constructor(){
+    this.produtos.push(
+      new Produto("Bolo de Fubá com Goiabada", "Bolo de Fubá recheado com pedaços de goiabada", "https://static.ifood-static.com.br/image/upload/t_medium/pratos/5b42c921-322f-47d5-b29b-fefe5ed0e332/202202140917_3P0C_i.jpg", 30.40),
+      new Produto("Bolo de Fubá com Goiabada", "Bolo de Fubá recheado com pedaços de goiabada", "https://static.ifood-static.com.br/image/upload/t_medium/pratos/5b42c921-322f-47d5-b29b-fefe5ed0e332/202202140917_3P0C_i.jpg", 30.40),
+      new Produto("Bolo de Fubá com Goiabada", "Bolo de Fubá recheado com pedaços de goiabada", "https://static.ifood-static.com.br/image/upload/t_medium/pratos/5b42c921-322f-47d5-b29b-fefe5ed0e332/202202140917_3P0C_i.jpg", 30.40),
+      new Produto("Bolo de Fubá com Goiabada", "Bolo de Fubá recheado com pedaços de goiabada", "https://static.ifood-static.com.br/image/upload/t_medium/pratos/5b42c921-322f-47d5-b29b-fefe5ed0e332/202202140917_3P0C_i.jpg", 30.40)
+    )
+  }
 }
